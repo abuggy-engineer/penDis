8bc9845c8010083e4c637b7b249c68f01910cd3f
abuggy-engineer <aodhanbuggy@gmail.com>
2023-04-05 14:46:56 +0100

    Multi-actuator control
    User defined control of 2 linear actuators

--- main/main.ino
+++ main/main.ino
@@ -1,40 +1,98 @@
 #include <Arduino.h>
 #include "linAct.h"
 
-linAct linAct;
+linAct pinPress_left;
+linAct pinPress_right;
 
 int counter = 0;
-int t0;
-double x0;
+
+double left_target, right_target;
 
 void setup() {
 
   Serial.begin(9600);
   Serial.println("Hello World");
-  linAct.init();
-
-  //linAct.calibrate();
+  
+  pinPress_left.set_pins(6, 7, 8, A0);
+  pinPress_left.init();
+  
+  pinPress_right.set_pins(13, 12, 11, A1);
+  pinPress_right.init();
 
 }
 
 void loop() {
 
-  while(Serial.available() == false){}
+  //Appreciate Left pin press initial stroke 28.5mm, right is 22.5 ~6mm diff
 
-  float target = Serial.parseFloat();
+  Serial.println("1. Dual Control \n2. Individual Control (6mm diff)");
+  while(!Serial.available()){}
+  int response = Serial.parseInt();
+  Serial.println(response);
 
-  if(target == 999)
+  switch(response)
   {
-    linAct.stop();
-    Serial.println("STOP");
-    return;
+    case 1:
+
+      Serial.println("Enter Target Position Pin Press Left: ");
+      Serial.println("222 = back \n333 = fwd \n444 = stop \n555 = print pos");
+      while(!Serial.available()){}
+  	  left_target = Serial.parseFloat();
+      Serial.println(left_target);
+
+      if(left_target == 222)
+      {
+        pinPress_left.move(-255);
+        pinPress_right.move(-255);
+      }
+      else if(left_target == 333)
+      {
+        pinPress_left.move(255);
+        pinPress_right.move(255);
+      }
+      else if(left_target == 444)
+      {
+        pinPress_left.stop();
+        pinPress_right.stop();
+      }
+      else if(left_target == 555)
+      {
+        int l = pinPress_left.readPos();
+        l = pinPress_left.pos2stroke(l);
+        Serial.print("Left: ");
+        Serial.print(l);
+        
+        int r = pinPress_left.readPos();
+        r = pinPress_left.pos2stroke(r);
+        Serial.print(" Right: ");
+        Serial.println(r);
+      }
+      else
+      {
+        pinPress_left.go2pos(left_target);
+        pinPress_right.go2pos(left_target + 6);
+      }
+
+    break;
+
+    case 2:
+      
+      Serial.println("Enter Target Position Pin Press Left: ");
+      while(!Serial.available()){}
+  	  left_target = Serial.parseFloat();
+      Serial.println(left_target);
+
+      Serial.println("Enter Target Position Pin Press Right: ");
+      while(!Serial.available()){}
+  	  right_target = Serial.parseFloat();
+      Serial.println(right_target);
+
+      pinPress_left.go2pos(left_target);
+      pinPress_right.go2pos(right_target);
+
+    break;
+    
+    default:
+      Serial.println("*******Incorrect Input*******");
   }
-  else if(target == 222)
-    linAct.move(-255);
-  else if(target == 333)
-    linAct.move(255);
-  else if(target == 444)
-    linAct.go2pos(0);
-  else if(target != 0)
-    linAct.go2pos(target);
 }
